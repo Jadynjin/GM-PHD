7fc07fdd53331af86179b640a86ca5282aa03491
milkhouse <milkhouse1990@gmail.com>
2019-06-18 22:57:26 +0800

    draft update

--- EM.py
+++ EM.py
@@ -3,25 +3,37 @@
 import numpy as np
 from numpy.random import randn
 
+from SqrtKalman import sqrt_kalman_smoother
+
 max_iterations = 100
 min_ll_decrease = 1e-6 # Min decrease of log-likelihood
 guess = 0.1 # Initial guess for the parameter
-true = 0.9
 
 # --- REAL SYSTEM ---
+A = 0.9
 N = 500
 x = np.zeros(N+1)
 y = np.zeros(N)
+C = 0.5
+Q = 0.1
+R = 0.1
+
+X1 = 0
+P1 = 0
 for t in range(N):
-    x[t+1] = true * x[t] + 0.1**0.5*randn(N)
-    y[t] = 0.5 * x[t] + 0.1**0.5*randn(N)
+    x[t+1] = A * x[t]+ Q**0.5*randn()
+    y[t] = C * x[t] + R**0.5*randn()
+
+from scipy.io import loadmat
+x = loadmat('EMLinSysId/x.mat')['x'][0,:]
+y = loadmat('EMLinSysId/y.mat')['y'][0,:]
 
 # --- EM ALGORITHM ---
 LL = []
 a = [guess]
 for k in range(max_iterations):
     # E step
-    g_LL = ks() # TODO
+    g_LL = sqrt_kalman_smoother(y, guess, C, Q, R, X1, P1, N) # TODO
     LL.append(-0.5*g_LL)
     # TODO
 
--- SqrtKalman.py
+++ SqrtKalman.py
@@ -1,22 +1,41 @@
-from scipy.linalg import inv
+import numpy as np
+from scipy.linalg import inv, cholesky
 
-def sqrt_kalman_smoother():
-    x_prior[0] = X1
-    P_sqrt_prior[0] = P1_sqrt
-
+def sqrt_kalman_smoother(z, A, C, Q, R, X1, P1, N):
     # Get the dimensions
     n = X1.shape[0]
     m = z.shape[0]
+
+    x_prior = np.zeros((N+1, n))
+    x_prior[0] = X1
+    x = np.zeros((N,n))
+
+    P_sqrt_prior = np.zeros((N+1, n, n))
+    P_sqrt_prior[0] = cholesky(P1)
+    P_sqrt = np.zeros(N,n,n)
+
+    Q_sqrt = cholesky(Q)
+    R_sqrt = cholesky(R)
+
+    R1 = np.zeros((n+m,n+m))
+    R2 = np.zeros((n+m,n+m))
+    R3 = np.zeros((n*2,n))
+    R4 = np.zeros((n*2,n))
+
+    K = np.zeros((N,n,m))
+    z_prior = np.zeros(N,m) 
+    z_postier = np.zeros(N,m)
+    residual = np.zeros(N,m)
+    error = np.zeros(N,m)
 
     # Kalman filter
     for t in range(N):
         # --- UPDATE PHASE ---
         # Covariance update
-        # R1 = np.zeros((n+m, n+m))
+        #
         # qr [R_sqrt,       0  ] = [S.T, (K * S.T).T   ]
         #    [P_sqrt_prior, C.T]   [0,   P_sqrt_postier]
         R1[:m,:m] = R_sqrt
-        R1[:m,m:] = np.zeros((m,n))
         R1[m:,:m] = P_sqrt_prior @ C.T
         R1[m:,m:] = P_sqrt_prior
 
@@ -51,11 +70,22 @@ def sqrt_kalman_smoother():
 
     # Smooth
     # Initialization
+    x_smooth = np.zeros((N+1,n))
     x_smooth[N] = x_prior[N].copy()
     x_smooth[N-1] = x[N-1].copy()
+
+    P_sqrt_smooth = np.zeros((N+1,n,n))
     P_sqrt_smooth[N] = P_sqrt_prior[N].copy()
     P_sqrt_smooth[N-1] = P_sqrt[N-1].copy()
+
+    M_smooth = np.zeros(N,n,n)
     M_smooth[N-1] = P_sqrt[N].T @ P_sqrt[N] @ A.T
+
+    z_smooth = np.zeros((N,m))
+    error_smooth = np.zeros((N,m))
+
+    R5 = np.zeros((3*n,2*n))
+    R6 = np.zeros((3*n,2*n))
 
     for t in range(N-2,-1,-1):
         P = P_sqrt[t] @ P_sqrt[t].T
@@ -87,7 +117,7 @@ def sqrt_kalman_smoother():
     H_xx = np.zeros((n,n))
     H_xx1 = np.zeros((n,n))
     H_x1x1 = np.zeros((n,n))
-    for i in range(N):
+    for t in range(N):
         H_xx += x_smooth[t] @ x_smooth[t].T + P_sqrt_smooth[t].T @ P_sqrt_smooth[t]
         H_xx1 += x_smooth[t] @ x_smooth[t+1].T + M_smooth[t]
         H_x1x1 += x_smooth[t+1] @ x_smooth[t+1].T + P_sqrt_smooth[t+1] @ P_sqrt_smooth[t+1].T
